d0ebeffb39f713a479e7c3f0a801e94b8bbc3e12
Tumble <tumblegamer@gmail.com>
2020-10-13 23:37:43 +0100

    first commit

---
+++ mod-utils.js
@@ -0,0 +1,47 @@
+window = window||unsafeWindow;
+var BCModUtils = (function() {
+	"use strict";
+
+	async function runIfDocLoaded(func) {
+		if (document.readyState == "complete") {
+			return new Promise(async (resolve, reject) => {
+				window.addEventListener("load", async () => {
+					resolve(await func());
+				})
+			})
+		}
+		return await func();
+	}
+
+	
+function camelize(str) {
+	return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+		if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+		return index === 0 ? match.toLowerCase() : match.toUpperCase();
+	});
+}
+
+function log(mod,...p) {
+	p.unshift("["+mod.acronym+"]");
+	console.debug(...p)
+}
+
+	function InitialiseMod({name,acronym,author}) {
+		console.log("[" + name + "] by " + author)
+		var mod = {
+			id:camelize(name),
+			acronym,
+			name,
+			author
+		}
+		mod.log = log.bind(mod.id)
+		if(cardboard) {
+			mod = cardboard.regsiter(mod.id,mod);
+		}
+	}
+	return {
+		InitialiseMod,
+		camelize,
+		runIfDocLoaded
+	}
+})();
