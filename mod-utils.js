e974fd6074c7554859f0a8925cf4794b14f7bec6
Tumble <tumblegamer@gmail.com>
2020-10-16 02:29:43 +0100

    ok big object

--- mod-utils.js
+++ mod-utils.js
@@ -1,61 +1,44 @@
-var BCModUtils = (function() {
-	"use strict";
+"use strict";
+var BCModUtils = {
 
-	function onDocumentLoaded() {
-		return new Promise((res,rej)=>{
-			if(document.readyState==="complete") {
+	onDocumentLoaded: function () {
+		return new Promise((res, rej) => {
+			if (document.readyState === "complete") {
 				res();
 			} else {
-				window.addEventListener("load",res)
+				window.addEventListener("load", res)
 			}
 		})
-	}
+	},
+	camelize: function (str) {
+		return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+			if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+			return index === 0 ? match.toLowerCase() : match.toUpperCase();
+		});
+	},
 
-	
-function camelize(str) {
-	return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-		if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-		return index === 0 ? match.toLowerCase() : match.toUpperCase();
-	});
-}
-
-function mapArguments(keys,argv,defaultKey) {
-	if(!Array.isArray(keys)) keys =Object.keys(keys);
-	defaultKey = defaultKey||keys[0]
-	if(!Array.isArray(argv)) return argv;
-	if(argv.length==1) return {[defaultKey]:argv[0]}
-	return keys.reduce((obj,key,i)=>(obj[key]=argv[i],obj),{})
-}
-
-function varDefined(v) {
-	return typeof(v)!=="undefined"&&v;
-}
-
-function log(mod,...p) {
-	p.unshift("["+mod.abriv+"]");
-	console.debug(...p)
-}
-
-
-	function InitialiseMod({id,name,abriv,author}) {
-		console.log("[" + name + "] by " + author)
+	mapArguments: function (keys, argv, defaultKey) {
+		if (!Array.isArray(keys)) keys = Object.keys(keys);
+		defaultKey = defaultKey || keys[0]
+		if (!Array.isArray(argv)) return argv;
+		if (argv.length == 1) return { [defaultKey]: argv[0] }
+		return keys.reduce((obj, key, i) => (obj[key] = argv[i], obj), {})
+	},
+	InitialiseMod: function ({ id, name, abriv, author }) {
+		console.log(`[${name}] by ${author}`)
 		var mod = {
-			id:camelize(id||name),
-			abriv:abriv||name.split(" ").map(word=>word[0].toUpperCase()).join(""),
+			id: id || camelize(name),
+			abriv: abriv || name.split(" ").map(word => word[0].toUpperCase()).join(""),
 			name,
-			author
+			author,
+			log:function (...p) {
+				p.unshift("[" + mod.abr,iv + "]");
+				console.debug(...p)
+			}
 		}
-		mod.log = log.bind(self,mod)
-		if(typeof(cardboard)!=="undefined") {
-			mod = cardboard.register(mod.id,mod);
+		if (typeof (cardboard) != "undefined") {
+			mod = cardboard.register(mod.id, mod);
 		}
 		return mod;
 	}
-	return {
-		InitialiseMod,
-		camelize,
-		onDocumentLoaded,
-		varDefined,
-		mapArguments
-	}
-})();
+}
